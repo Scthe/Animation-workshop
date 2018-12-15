974ca2784010b66cb11e9c2b0669721e268c74e4
Scthe <marcin1113C@gmail.com>
2018-12-15 21:19:42 +0100

    Create prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
 const ROOT = path.resolve( __dirname, 'src' );
 const DESTINATION = path.resolve( __dirname, 'dist' );
 const ASSETS = path.resolve( __dirname, 'assets' );
 const SHADERS = path.resolve( ROOT, 'shaders' );
 
-module.exports = {
+const config = ({mode, isProd}) => ({
+    mode,
+
     context: ROOT,
 
     entry: {
@@ -16,6 +23,11 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: DESTINATION
+    },
+
+    // webpack's default recommended entry point size is 244kb. This app is 246kb.
+    performance: {
+      maxEntrypointSize: 250 * 1024, // 250kb
     },
 
     resolve: {
@@ -42,13 +54,76 @@ module.exports = {
             { test: /\.(png|jpg|gif)$/, use: 'url-loader?limit=15000&name=[name]-[hash].[ext]', exclude: /node_modules/ },
             { test: /\.scss$/, exclude: /node_modules/,
               loaders: [
-                'style-loader', // creates style nodes from JS strings
-                'css-loader?modules&namedExport&localIdentName=[local]--[hash:base64:5]', // translates CSS into CommonJS
+                (isProd
+                  ? MiniCssExtractPlugin.loader // extracts to file
+                  : 'style-loader' // creates <style> nodes from JS strings
+                ),
+                {
+                  loader: 'css-loader', // translates CSS into CommonJS
+                  options: {
+                    modules: true,
+                    namedExport: true,
+                    localIdentName: isProd ? '[hash:base64:4]' : '[local]--[hash:base64:5]',
+                  }
+                },
                 'sass-loader' // compiles Sass to CSS, using Node Sass by default
               ] }
         ],
     },
 
-    devtool: 'cheap-module-source-map',
-    devServer: {}
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': { // just in case, webpack4 changed this. (argv.mode is not very documented, so I don't trust docs for this)
+          'NODE_ENV': JSON.stringify(mode),
+          'DEBUG': !isProd,
+        }
+      }),
+      new HtmlWebpackPlugin({ // create HTML file as part of webpack output. Also injects all chunks
+        template: 'index.html',
+        sourceMap: false,
+      }),
+    ],
+
+});
+
+const configProd = config => {
+  // no need to gzip, as github pages has it's own rules
+  config.optimization = {
+   minimizer: [
+     new TerserPlugin({ // uglify does not handle ES6. We need minifier as we also override CSS optimizer
+       parallel: true,
+     }),
+     new OptimizeCSSAssetsPlugin({}),
+   ],
+ };
+
+  config.plugins.unshift(
+    new CleanWebpackPlugin([`${DESTINATION}/*`]), // remove files from dist/
+    new MiniCssExtractPlugin({ // create separate .css file
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  );
+
+  config.stats = { children: false }; // MiniCssExtractPlugin spam..
+
+  return config;
 };
+
+const configDev = config => {
+  config.devServer = {};
+  config.devtool = 'cheap-module-source-map';
+  return config;
+};
+
+module.exports = (env, argv) => {
+  const mode = argv.mode;
+  const opts = { mode, isProd: mode === 'production' };
+  console.log('MODE:', JSON.stringify(opts));
+
+  const cfg = config(opts);
+
+  return opts.isProd
+    ? configProd(cfg)
+    : configDev(cfg);
+}
